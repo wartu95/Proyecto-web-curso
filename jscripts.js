849436cc7664d2769b08d35fe77b31deedab6d8b
wartu95 <113658802+wartu95@users.noreply.github.com>
2023-07-27 13:30:55 -0500

    Cambiar las comillas

--- jscripts.js
+++ jscripts.js
@@ -1,4 +1,4 @@
 function mostrarSaludo(){
   const nombre= prompt("Ingresa tu nombre: ");
-  alert('¡Hola, ${nombre}! Bienvenido(a) a nuestro sitio web.');
+  alert(`¡Hola, ${nombre}! Bienvenido(a) a nuestro sitio web.`);
 }
